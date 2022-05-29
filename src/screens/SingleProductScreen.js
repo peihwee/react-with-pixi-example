ff09e48f7a697692b5cc1e00c4a4f267141119a7
peihwee <puaywee@gmail.com>
2022-05-29 13:43:01 +0800

    add tween

--- src/screens/SingleProductScreen.js
+++ src/screens/SingleProductScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import * as PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js';
 import ResizablePixiApp from '../pixi/ResizablePixiApp';
 import Popup from '../components/Popup';
+
+import { gsap } from "gsap";
+import { PixiPlugin } from "gsap/PixiPlugin";
 
 class SingleProductScreen extends React.Component
 {
     constructor() {
-        super()
+        super();
 
         this.divPixi = null;
 
@@ -22,6 +25,13 @@ class SingleProductScreen extends React.Component
     }
     componentDidMount() 
     {
+        /*
+        // register the plugin
+        gsap.registerPlugin(PixiPlugin);
+
+        // give the plugin a reference to the PIXI object
+        PixiPlugin.registerPIXI(PIXI);
+        */
         this.objResizable = new ResizablePixiApp(this.divPixi);
         this.objPixiApp = this.objResizable.getPixiApp();
         
@@ -49,24 +59,43 @@ class SingleProductScreen extends React.Component
             sprFish.x = 50 + Math.random() * (this.objResizable.width - 100);
             sprFish.y = 50 + Math.random() * (this.objResizable.height - 100);
 
-            sprFish.scale.x = sprFish.scale.y = 0.15 + (Math.random() * 0.2);
+            sprFish.iScale = sprFish.scale.x = sprFish.scale.y = 0.15 + (Math.random() * 0.2);
             
             this.objPixiApp.stage.addChild(sprFish);
 
             const swimSpeed = 3 + (Math.random() * 6);
 
+            /*
             this.objPixiApp.ticker.add(() => {
                 sprFish.x -= swimSpeed;
 
                 if(sprFish.x + sprFish.width < 0) sprFish.x = this.objResizable.width + 50;
             });
+            */
 
+            sprFish.fThink = () => 
+            {
+                let randomX = 50 + Math.random() * (this.objResizable.width - 100);
+                let randomY = 50 + Math.random() * (this.objResizable.height - 100);
+                let randomDuration = 3 + (Math.random() * 20);
+                //console.log(sprFish);
+
+                if(randomX > sprFish.x) sprFish.scale.x = -1 * sprFish.iScale;
+                else sprFish.scale.x = 1 * sprFish.iScale;
+                
+                gsap.to(sprFish.position, { x: randomX, y: randomY, duration: randomDuration, onComplete: sprFish.fThink } );
+            }
+
+            sprFish.fThink();
+            
+            //gsap.to(sprFish, { x: randomX, y: randomY, duration: randomDuration, onComplete: sprFish.fThink } );
+            
             sprFish.iID = i;
             sprFish.iColor = iColor;
 
             sprFish.interactive = true;
-            sprFish.on('pointerdown', (event) => {
-
+            sprFish.on('pointerdown', (event) => 
+            {
                 ///////////////////////////////////////////////////////////////////////////
                 // Convert global to follow stage position
                 ///////////////////////////////////////////////////////////////////////////
